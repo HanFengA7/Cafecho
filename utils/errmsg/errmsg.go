37eef73b40f501edc71144ad01ebaafebfdb0d1c
HanFengA7 <tancan137@foxmail.com>
2022-12-28 11:32:07 +0800

    [init]:错误处理

---
+++ utils/errmsg/errmsg.go
@@ -0,0 +1,30 @@
+package errmsg
+
+const (
+	SUCCESS = 200
+	ERROR   = 500
+
+	ErrorUsernameUsed      = 1001
+	ErrorUserPasswordWrong = 1002
+	ErrorUserNotExist      = 1003
+	ErrorTokeExist         = 1004
+	ErrorTokeRuntime       = 1005
+	ErrorTokeWrong         = 1006
+	ErrorTokeTypeWrong     = 1007
+)
+
+var CodeMsg = map[int]string{
+	SUCCESS:                "OK",
+	ERROR:                  "FAIL",
+	ErrorUsernameUsed:      "用户名已存在",
+	ErrorUserPasswordWrong: "用户密码错误",
+	ErrorUserNotExist:      "用户不存在",
+	ErrorTokeExist:         "TOKE不存在",
+	ErrorTokeRuntime:       "TOKE已过期",
+	ErrorTokeWrong:         "TOKE不正确",
+	ErrorTokeTypeWrong:     "TOKE格式错误",
+}
+
+func GetErrMsg(code int) string {
+	return CodeMsg[code]
+}
