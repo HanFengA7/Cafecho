4fe12c84221fa20f92897621285b99b14b41e2e5
HanFengA7 <tancan137@foxmail.com>
2022-12-28 11:58:25 +0800

    [init]:UserModel RouterV1 Api

--- api/v1/user.go
+++ api/v1/user.go
@@ -2,32 +2,32 @@ package v1
 
 import "github.com/gin-gonic/gin"
 
-// 增加用户
+// AddUser 增加用户
 func AddUser(c *gin.Context) {
 
 }
 
-// 查询用户是否存在
+// UserExist 查询用户是否存在
 func UserExist(c *gin.Context) {
 
 }
 
-// 查询单个用户
+// GetUser 查询单个用户
 func GetUser(c *gin.Context) {
 
 }
 
-// 查询用户列表
+// GetUsers 查询用户列表
 func GetUsers(c *gin.Context) {
 
 }
 
-// 编辑用户
+// EditUser 编辑用户
 func EditUser(c *gin.Context) {
 
 }
 
-// 删除用户
+// DeleteUser 删除用户
 func DeleteUser(c *gin.Context) {
 
 }
--- routes/router.go
+++ routes/router.go
@@ -13,10 +13,17 @@ func InitRouter() {
 	RouterV1 := router.Group("api/v1")
 	{
 		//UserModel RouterV1 Api
+		//增加用户
 		RouterV1.POST("user/add", v1.AddUser)
+		// 查询单个用户
+		RouterV1.GET("user", v1.GetUser)
+		// 查询用户列表
 		RouterV1.GET("users", v1.GetUsers)
+		// 编辑用户
 		RouterV1.PUT("user/:id", v1.EditUser)
+		// 删除用户
 		RouterV1.DELETE("user/:id", v1.DeleteUser)
+
 		//CategoryModel RouterV1 Api
 
 		//ArticleModel RouterV1 Api
