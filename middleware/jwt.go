9a34ec024ebdf132f2e4132f87203b3734d8454e
HanFengA7 <tancan137@foxmail.com>
2022-12-31 11:55:50 +0800

    [init]:JWT

--- middleware/jwt.go
+++ middleware/jwt.go
@@ -1,9 +1,52 @@
 package middleware
 
-import "Cafecho/utils"
+import (
+	"Cafecho/utils"
+	"Cafecho/utils/errmsg"
+	"github.com/golang-jwt/jwt/v4"
+	"time"
+)
 
 var JwtKey = []byte(utils.JwtKey)
 
 type MyClaims struct {
 	Username string `json:"username"`
+	jwt.RegisteredClaims
+	ExpiresAt *jwt.NumericDate
+	IssuedAt  *jwt.NumericDate
+	NotBefore *jwt.NumericDate
+	Issuer    string
 }
+
+// SetToken 生成Token
+func SetToken(username string) (string, int) {
+	ExpiresTime := jwt.NewNumericDate(time.Now().Add(24 * time.Hour))
+	RegisteredClaims := MyClaims{
+		ExpiresAt: ExpiresTime,
+		IssuedAt:  jwt.NewNumericDate(time.Now()),
+		NotBefore: jwt.NewNumericDate(time.Now()),
+		Issuer:    "Cafecho",
+	}
+
+	reqClaims := jwt.NewWithClaims(jwt.SigningMethodHS256, RegisteredClaims)
+	token, err := reqClaims.SignedString(JwtKey)
+	if err != nil {
+		return "", errmsg.ERROR
+	} else {
+		return token, errmsg.SUCCESS
+	}
+}
+
+// CheckToken 验证Token
+func CheckToken(token string) (*MyClaims, int) {
+	regToken, _ := jwt.ParseWithClaims(token, &MyClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return JwtKey, nil
+	})
+	if key, code := regToken.Claims.(*MyClaims); code && regToken.Valid {
+		return key, errmsg.SUCCESS
+	} else {
+		return nil, errmsg.ERROR
+	}
+}
+
+// JWT中间件
