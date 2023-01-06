10e7c95631022b543a5fd69d49ea36290b9a6de6
HanFengA7 <tancan137@foxmail.com>
2023-01-06 18:17:02 +0800

    [fix]:Cors Bug

--- middleware/cors.go
+++ middleware/cors.go
@@ -7,13 +7,12 @@ import (
 )
 
 func Cors() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		cors.New(cors.Config{
-			AllowOrigins:  []string{"*"},
-			AllowMethods:  []string{"*"},
-			AllowHeaders:  []string{"Origin"},
-			ExposeHeaders: []string{"Content-Length", "Authorization"},
-			MaxAge:        12 * time.Hour,
-		})
-	}
+	return cors.New(cors.Config{
+		AllowOrigins:     []string{"*"}, // 等同于允许所有域名 #AllowAllOrigins:  true
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"*", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length", "text/plain", "Authorization", "Content-Type", "Access-Control-Allow-Origin", "*"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	})
 }
