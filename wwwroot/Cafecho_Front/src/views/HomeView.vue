f5824b3d8b84e0f0054a46c70095277df831c6a9
HanFengA7 <tancan137@foxmail.com>
2023-04-11 23:46:52 +0800

    <feat>:前端UI

--- wwwroot/Cafecho_Front/src/views/HomeView.vue
+++ wwwroot/Cafecho_Front/src/views/HomeView.vue
@@ -80,6 +80,141 @@
                         </a-space>
                     </a-row>
                 </div>
+
+
+                <div class="IndexBox-1">
+
+                    <a-row :gutter="{ md: 8, lg: 24, xl: 32 }">
+                        <a-col :span="6">
+                            <a-card :bordered="false" class="BoxCard" hoverable>
+                                    <span>
+                                    <a-avatar
+                                        :size="40"
+                                        shape="square"
+                                    >
+                                        <IconHeart/>
+                                    </a-avatar>
+                                    <a-typography-text>关 于</a-typography-text>
+                                    </span>
+                            </a-card>
+                        </a-col>
+                        <a-col :span="6">
+                            <a-card :bordered="false" class="BoxCard" hoverable>
+                                    <span>
+                                    <a-avatar
+                                        :size="40"
+                                        shape="square"
+                                    >
+                                        <IconMindMapping/>
+                                    </a-avatar>
+                                    <a-typography-text>计 划</a-typography-text>
+                                    </span>
+                            </a-card>
+                        </a-col>
+                        <a-col :span="6">
+                            <a-card :bordered="false" class="BoxCard" hoverable>
+                                    <span>
+                                    <a-avatar
+                                        :size="40"
+                                        shape="square"
+                                    >
+                                        <IconUserGroup/>
+                                    </a-avatar>
+                                    <a-typography-text>朋 友</a-typography-text>
+                                    </span>
+                            </a-card>
+                        </a-col>
+                    </a-row>
+                </div>
+
+                <div class="IndexBox-2">
+                    <a-row :gutter="24">
+                        <a-col :span="20">
+                            <div>
+
+                                <a-timeline>
+                                    <a-timeline-item>
+                                        <a-card>
+                                            <template #actions>
+                                                <span class="icon-hover"> <IconThumbUp/> </span>
+                                                <span class="icon-hover"> <IconShareInternal/> </span>
+                                                <span class="icon-hover"> <IconFire/> </span>
+                                            </template>
+                                            <template #cover>
+                                                <div
+                                                    :style="{height: '204px',overflow: 'hidden',}"
+                                                >
+                                                    <img
+                                                        alt="dessert"
+                                                        src="https://i.stay.pub/xj/32a4f6b503c16426.jpg"
+                                                        style="height: 100%;width: 100%;object-fit: cover"
+                                                    />
+                                                </div>
+                                            </template>
+                                            <a-card-meta description="This is the description"
+                                                         title="烟雨朦胧迷糊景，风舞翩跹醉春情">
+                                                <template #avatar>
+                                                    <div
+                                                        :style="{ display: 'flex', alignItems: 'center', color: '#1D2129' }"
+                                                    >
+                                                        <a-avatar :size="24" :style="{ marginRight: '8px' }">
+                                                            <img alt="avatar"
+                                                                 src="https://q1.qlogo.cn/g?b=qq&nk=838214611&s=640"/>
+                                                        </a-avatar>
+                                                        <a-typography-text>Cosanoxj</a-typography-text>
+                                                    </div>
+                                                </template>
+                                            </a-card-meta>
+                                        </a-card>
+                                        <br/>
+                                        <br/>
+                                    </a-timeline-item>
+                                    <a-timeline-item>
+                                        <a-card>
+                                            <template #actions>
+                                                <span class="icon-hover"> <IconThumbUp/> </span>
+                                                <span class="icon-hover"> <IconShareInternal/> </span>
+                                                <span class="icon-hover"> <IconFire/> </span>
+                                            </template>
+                                            <template #cover>
+                                                <div
+                                                    :style="{height: '204px',overflow: 'hidden',}"
+                                                >
+                                                    <img
+                                                        alt="dessert"
+                                                        src="https://cdnjson.com/images/2023/02/09/ap.webp"
+                                                        style="height: 100%;width: 100%;object-fit: cover"
+                                                    />
+                                                </div>
+                                            </template>
+                                            <a-card-meta description="This is the description"
+                                                         title="烟雨朦胧迷糊景，风舞翩跹醉春情">
+                                                <template #avatar>
+                                                    <div
+                                                        :style="{ display: 'flex', alignItems: 'center', color: '#1D2129' }"
+                                                    >
+                                                        <a-avatar :size="24" :style="{ marginRight: '8px' }">
+                                                            <img alt="avatar"
+                                                                 src="https://q1.qlogo.cn/g?b=qq&nk=838214611&s=640"/>
+                                                        </a-avatar>
+                                                        <a-typography-text>Cosanoxj</a-typography-text>
+                                                    </div>
+                                                </template>
+                                            </a-card-meta>
+                                        </a-card>
+                                        <br/>
+                                        <br/>
+                                    </a-timeline-item>
+                                </a-timeline>
+
+                            </div>
+                        </a-col>
+                        <a-col :span="4">
+                            <div></div>
+                        </a-col>
+                    </a-row>
+                </div>
+
             </a-layout-content>
             <a-layout-footer></a-layout-footer>
         </a-layout>
@@ -93,10 +228,16 @@ import {
     IconCalendar,
     IconCaretLeft,
     IconCaretRight,
+    IconFire,
     IconGithub,
+    IconHeart,
     IconHome,
+    IconMindMapping,
     IconQqCircleFill,
+    IconShareInternal,
     IconSunFill,
+    IconThumbUp,
+    IconUserGroup,
 } from '@arco-design/web-vue/es/icon';
 
 const collapsed = ref(false);
@@ -104,3 +245,26 @@ const onCollapse = () => {
     collapsed.value = !collapsed.value;
 };
 </script>
+
+<style scoped>
+.IndexBox-1 {
+    /*padding: 25px 400px;*/
+    transform: translateY(-50%);
+    padding: 0px 400px;
+}
+
+.IndexBox-1 .BoxCard {
+    background-color: #dfe0e23b;
+    border-radius: 10px;
+}
+
+.IndexBox-1 .BoxCard span {
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+}
+
+.IndexBox-2 {
+    padding: 25px 250px;
+}
+</style>
